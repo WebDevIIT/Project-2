4a009e018b0207a47305e94bfe6cfe743158f209
dekay7 <darshkapadia@gmail.com>
2024-04-24 23:46:58 -0500

    fixed indent

--- js/form_validation.js
+++ js/form_validation.js
@@ -1,15 +1,15 @@
 function validateForm() {
-    var email = document.forms["signupForm"]["email"].value;
-    var emailRegex = /\S+@\S+\.\S+/;
-    
-    if (email == "") {
-      alert("Email address must be filled out");
-      return false;
-    } else if (!emailRegex.test(email)) {
-      alert("Please enter a valid email address");
-      return false;
-    } else {
-      alert("Your email, " + email + " has been subscribed to our newsletter!");
-      return true;
-    }
+  var email = document.forms["signupForm"]["email"].value;
+  var emailRegex = /\S+@\S+\.\S+/;
+  
+  if (email == "") {
+    alert("Email address must be filled out");
+    return false;
+  } else if (!emailRegex.test(email)) {
+    alert("Please enter a valid email address");
+    return false;
+  } else {
+    alert("Your email, " + email + " has been subscribed to our newsletter!");
+    return true;
+  }
   }
